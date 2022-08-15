6e0b0d04e64a3c615b5b6529a1b3ae90df25c4ee
denosaur98 <109144817+denosaur98@users.noreply.github.com>
2022-08-15 16:33:20 +0300

    Update Задача 2.js

--- module7_homework/Задача 2.js
+++ module7_homework/Задача 2.js
@@ -1,8 +1,13 @@
-function obj() {
-    let objProt = Object.create(null);
-    return objProt;}
-  console.log(obj());
-  
-  /*
-  Функция, которая создает пустой объект без прототипа
-  */
+const obj = {a: 1, b: true, c: 'string'};
+let key = "..."
+function inObj(key, obj) {
+  if (key in obj) {
+    return true;
+  } else {
+    return false;
+  }
+}
+console.log(inObj(key, obj));
+/*
+Функция, которая принимает в качестве аргументов строку и объект, а затем проверяет есть ли у переданного объекта свойство с данным именем. Функция возвращает true или false.
+*/
